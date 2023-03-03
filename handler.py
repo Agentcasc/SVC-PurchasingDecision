2d55641e21d66c4d83c7cb58f5a1fcbdf4f9f179
Yanjie Mac <yanjiezheng@berkeley.edu>
2023-03-03 09:34:48 -0800

    a

--- handler.py
+++ handler.py
@@ -1,16 +1,42 @@
-
+import pandas as pd
 import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+import paramiko
 
+
+ssh_host = 'ec2-18-208-120-207.compute-1.amazonaws.com'
+ssh_username = 'ubuntu'
+ssh_password = 'SecretPassword'
+database_name = 'datacrunch'
 endpoint = 'mwsmaster-dtc-r.cfx5gks3rioa.us-east-1.rds.amazonaws.com'
-username = 'yanjie_zheng'
-password = '4pGp37bvNnjHEejb'
-database_name = '*'
+database_username = 'yanjie_zheng'
+database_password = '4pGp37bvNnjHEejb'
 
 
-connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-cursor = connection.cursor()
-cursor.execute("SELECT VERSION()")
-data = cursor.fetchone()
-print ("Database version : %s " % data)
-#%%
+pkey='../data_science.pem'
+key=paramiko.RSAKey.from_private_key_file(pkey)
+
+def open_ssh_tunnel(verbose=False):
+    """Open an SSH tunnel and connect using a username and password.
+
+    :param verbose: Set to True to show logging
+    :return tunnel: Global SSH tunnel connection
+    """
+
+    if verbose:
+        sshtunnel.DEFAULT_LOGLEVEL = logging.DEBUG
+
+    global tunnel
+    tunnel = SSHTunnelForwarder(
+        (ssh_host, 22),
+        ssh_username=ssh_username,
+        ssh_pkey=ssh_password,
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+
+    sshTunne
+
+    tunnel.start()
